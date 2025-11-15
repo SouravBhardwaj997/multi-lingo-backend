9870dcbc118ea741f825c78256bb3c9c7558cc83
Sourav <Souravbhardwaj997@gmail.com>
2025-11-15 18:07:19 +0530

    Setup DB

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 import app from "./app";
+import connectDB from "./lib/db";
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
+  connectDB();
   console.log(`Server running on port ${PORT}`);
 });
